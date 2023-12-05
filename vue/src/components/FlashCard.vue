b19f5f3c8056cf14722526dc1ee22ea677b8fcce
Koby McGuire <mcguirej2019@gmail.com>
2023-12-05 12:59:35 -0500

    edit cards

--- vue/src/components/FlashCard.vue
+++ vue/src/components/FlashCard.vue
@@ -10,8 +10,15 @@
         <h1>Answer:</h1>
         <p>{{ flashcard.answer }}</p>
       </div>
+
+      <div class="flashcard-tags" v-if="displayTags">
+        <p>{{ flashcard.tag }}</p>
+      </div>
     </div>
-    <div>
+
+    <div class="flashcard-buttons">
+      <button v-on:click="this.displayTags = !this.displayTags">View Tags</button>
+      <button v-on:click="displayModal">Edit Card</button>
       <button v-on:click="deleteFlashcard">Delete Card</button>
     </div>
   </div>
@@ -26,6 +33,13 @@ export default {
   data() {
     return {
       showFront: true,
+      editedFlashcard: {
+        question: this.flashcard.question,
+        answer: this.flashcard.answer,
+        tag: this.flashcard.tag,
+      },
+      editFlashcardError: false,
+      displayTags : false
     };
   },
   computed: {},
@@ -34,14 +48,60 @@ export default {
     flipCard() {
       this.showFront = !this.showFront;
     },
+
+
+    editFlashcard() {
+      console.log(this.flashcard.flashCardId);
+
+      // Check fields to make sure none are empty
+      if (
+        this.editedFlashcard.question === "" ||
+        this.editedFlashcard.answer === "" ||
+        this.editedFlashcard.tag === ""
+      ) {
+        this.editFlashcardError = true;
+        console.log(this.flashcard.flashCardId);
+        return;
+      }
+
+      // Apply "still-needed" fields to edited flashcard  
+      // this.editedFlashcard.flashCardId = this.flashcard.flashCardId;
+      // this.editedFlashcard.deckId = this.flashcard.deckId;
+      // this.editedFlashcard.creator = this.flashcard.creator;
+
+      // API call to update flashcard
+      DeckService.updateFlashcard(
+        // this.flashcard.flashCardId,
+        this.editedFlashcard.flashCardId,
+        this.editedFlashcard
+      )
+        .then((response) => {
+          // Close modal
+          this.closeModal();
+
+          // Reset edited flashcard fields
+          // this.editedFlashcard = {
+          //   flashCardId: "",
+          //   deckId: "",
+          //   question: "",
+          //   answer: "",
+          //   tag: "",
+          //   creator: "",
+          // };
+        })
+        .catch((error) => {
+          this.errorHandler(error, "updating flashcard");
+        });
+    },
+
+    // Delete Flashcard
     deleteFlashcard() {
-      console.log(this.flashcard);
       DeckService.deleteFlashcard(this.flashcard.flashCardId)
         .then((response) => {
           this.$router.go();
         })
         .catch((error) => {
-          this.errorHandler(error, "Flashcard Deleting");
+          this.errorHandler(error, "deleting flashcard");
         });
     },
     errorHandler(error, verb) {
@@ -52,9 +112,57 @@ export default {
 </script>
 
 <style scoped>
+/* The Modal */
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 30%; /* Could be more or less, depending on screen size */
+}
+
+.inner-modal-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.inner-modal-content label {
+  margin-top: 10px;
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
 .flashcard-card {
   min-width: 25%;
-  min-height: 20%;
+  min-height: 30%;
 
   border: 3px solid black;
   border-radius: 10px;
@@ -70,5 +178,15 @@ export default {
 
 h1 {
   font-size: large;
+}
+
+.flashcard-buttons {
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+}
+
+.submit-flashcard-edit {
+  margin-top: 10px;
 }
 </style>
