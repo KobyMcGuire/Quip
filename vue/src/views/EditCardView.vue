451baa59296c5e533b5fce6a7358d6a819f2ac0a
Koby McGuire <mcguirej2019@gmail.com>
2023-12-05 14:03:28 -0500

    Update flashcard

--- vue/src/views/EditCardView.vue
+++ vue/src/views/EditCardView.vue
@@ -1,5 +1,34 @@
 <template>
-  <h1> hello, the card question is {{ flashcard.question }} </h1>
+  <div class="edit-card-form">
+    <h1>Edit Flashcard Fields</h1>
+
+    <div class="input-fields">
+      <label for="newFlashcardQuestion">Question </label>
+      <input
+        type="text"
+        id="newFlashcardQuestion"
+        name="newFlashcardQuestion"
+        v-model="editedFlashcard.question"
+      />
+
+      <label for="newFlashcardAnswer">Answer </label>
+      <input
+        type="text"
+        id="newFlashcardAnswer"
+        name="newFlashcardAnswer"
+        v-model="editedFlashcard.answer"
+      />
+      <label for="newFlashcardQuestion">Tags </label>
+      <input
+        type="text"
+        id="newFlashcardQuestion"
+        name="newFlashcardQuestion"
+        v-model="editedFlashcard.tag"
+      />
+
+      <input class="submit-button" type="submit" v:on:click="updateCard" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,26 +38,33 @@ export default {
   data() {
     return {
       flashcard: {
-        flashCardId : "",
+        flashCardId: "",
         deckId: "",
         question: "",
         answer: "",
         tag: "",
-        creator: ""
+        creator: "",
+      },
+      editedFlashcard: {
+        flashCardId: "",
+        deckId: "",
+        question: "",
+        answer: "",
+        tag: "",
+        creator: "",
       },
     };
   },
 
   // API call to set flashcard
   created() {
-    DeckService.getCards()
-    .then((response) => {
-        let flashcards = response.data;
-
-    })
-    .catch((error) => {
+    DeckService.getCard(this.$route.params.id)
+      .then((response) => {
+        this.flashcard = response.data;
+      })
+      .catch((error) => {
         this.errorHandler(error, "fetching flashcard");
-    })
+      });
   },
 
   methods: {
@@ -36,9 +72,56 @@ export default {
     errorHandler(error, verb) {
       console.log(`There was an error ${verb}. The error was: ${error}`);
     },
-  },
-};
+
+    updateCard() {
+        // Check what form fields are empty
+        if (this.editedFlashcard.question === "") {
+            this.editedFlashcard.question = this.flashcard.question;
+        }
+
+        if (this.editedFlashcard.answer === "") {
+            this.editedFlashcard.answer = this.flashcard.answer;
+        }
+
+        if (this.editedFlashcard.tag === "") {
+            this.editedFlashcard.tag = this.flashcard.tag;
+        }
+    },
+  }
+}
 </script>
 
-<style>
+<style scoped>
+.edit-card-form {
+  margin-left: auto;
+  margin-right: auto;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 50%;
+  padding: 10px;
+
+  border-radius: 10px;
+
+  background-color: #f5f5f4;
+}
+
+.input-fields {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.submit-button {
+  margin-top: 10px;
+
+  max-width: 125px;
+}
+
+.edit-card-form input {
+  width: 200px;
+}
 </style>
